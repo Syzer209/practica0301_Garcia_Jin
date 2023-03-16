211a6abe0021b38a000b8405d260df19a45d3f74
Leyre <113433045+lgarciarob@users.noreply.github.com>
2023-03-16 17:14:28 +0100

    Ejercicio 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,36 +1,48 @@
 #RECURSIVA
+
 import datetime
 
 def fibonacci(n):
-   """
-   La función sirve para recibir un número entero positivo n
-   y calcule el número de fibonacci asociado a ese número con bucles.
-   Parametros:
-       - n: numero entero positivo introducido por el usuario
-   Salidas:
-       - numero_fibonacci: es el (numero-2)+(numero+1)
-   """
-   numero_fibonacci = (n - 2) + (n + 1)
-   return print(numero_fibonacci)
 
+  """
+  La función sirve para recibir un número entero positivo n
 
-start_time = datetime.datetime.now()
-fibonacci(1)
-end_time = datetime.datetime.now()
-print("El tiempo de ejecución es de: ", end_time - start_time)
+  y calcule el número de fibonacci asociado a ese número con bucles.
 
+  Parametros:
 
+      - n: numero entero positivo introducido por el usuario
+
+  Salidas:
+
+      - numero_fibonacci: es el (numero-2)+(numero+1)
+  """
+
+  if n == 0:
+      return 0
+  if n == 1:
+      return 1
+  return fibonacci(n-1) + fibonacci(n-2)
 
 #BUCLES
-import datetime
 
-def fibonacci(n):
+def fibonaccibucle(n):
 
-  numero_fibonacci = (n - 2) + (n + 1)
-  return print(numero_fibonacci)
+   a = 0
+   b = 1
 
+   for i in range(n):
+
+       c = b + a
+       a = b
+       b = c
+
+   return a
 
 start_time = datetime.datetime.now()
-fibonacci(1)
+
+print(fibonaccibucle(20))
+
 end_time = datetime.datetime.now()
+
 print("El tiempo de ejecución es de: ", end_time - start_time)
